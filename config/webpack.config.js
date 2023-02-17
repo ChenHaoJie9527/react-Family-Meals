1280cb3cb87489686de029714445f73e5af44475
tourl-ts <18312883857@163.com>
2023-02-18 00:09:18 +0800

    feat: 配置别名

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -310,6 +310,7 @@ module.exports = function (webpackEnv) {
         .map(ext => `.${ext}`)
         .filter(ext => useTypeScript || !ext.includes('ts')),
       alias: {
+        '@': paths.appSrc, // 配置别名 => src目录
         // Support React Native Web
         // https://www.smashingmagazine.com/2016/08/a-glimpse-into-the-future-with-react-native-for-web/
         'react-native': 'react-native-web',
