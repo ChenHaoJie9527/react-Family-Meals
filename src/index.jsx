2d7561fcc04e3fa25fc0c14b93f957a5c58af0f3
chenhaojie9527 <a18520145493@163.com>
2023-03-16 23:51:02 +0800

    feat: todolist

--- src/index.jsx
+++ src/index.jsx
@@ -7,7 +7,9 @@ import "react-app-polyfill/stable";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <div className="main">123</div>
+    <div className="main">
+      <h1 className="header">TODO</h1>
+    </div>
   </React.StrictMode>
 );
 
