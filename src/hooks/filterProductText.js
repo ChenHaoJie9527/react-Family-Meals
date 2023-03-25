ba5eab0a387cdeb21c2c775316a33a49e9f9babc
chenhaojie9527 <a18520145493@163.com>
2023-03-25 17:09:10 +0800

    feat: 过滤列表功能完成

--- src/hooks/filterProductText.js
+++ src/hooks/filterProductText.js
@@ -1,7 +1,10 @@
 // 通过关键字过滤产品列表
 function filterProductText(productName, productList = []) {
-  const res = productList.filter((item) => item.name === productName);
-  return res;
+  if (productName) {
+    const res = productList.filter((item) => item.name === productName);
+    return res;
+  }
+  return productList;
 }
 
 export { filterProductText };
--- src/test/index.spec.js
+++ src/test/index.spec.js
@@ -13,4 +13,35 @@ describe("filterProductText", () => {
     const filterList = filterProductText("aaa", list);
     expect(filterList).toEqual([]);
   });
+  it("Show all if no keywords", () => {
+    const filterList = filterProductText("", list);
+    expect(filterList).toEqual([
+      { category: "Fruits", price: "$1", stocked: true, name: "Apple" },
+      {
+        category: "Fruits",
+        price: "$1",
+        stocked: true,
+        name: "Dragonfruit",
+      },
+      {
+        category: "Fruits",
+        price: "$2",
+        stocked: false,
+        name: "Passionfruit",
+      },
+      {
+        category: "Vegetables",
+        price: "$2",
+        stocked: true,
+        name: "Spinach",
+      },
+      {
+        category: "Vegetables",
+        price: "$4",
+        stocked: false,
+        name: "Pumpkin",
+      },
+      { category: "Vegetables", price: "$1", stocked: true, name: "Peas" },
+    ]);
+  });
 });
