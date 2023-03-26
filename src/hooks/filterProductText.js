e6b04b8a8404752329caa14bc6c05550a2010cd4
chenhaojie9527 <a18520145493@163.com>
2023-03-26 23:24:31 +0800

    feat: Only show products in stock

--- src/hooks/filterProductText.js
+++ src/hooks/filterProductText.js
@@ -1,10 +1,25 @@
 // 通过关键字过滤产品列表
-function filterProductText(productName, productList = []) {
+function filterProductText(productName, productList = [], isStocked) {
   if (productName) {
-    const res = productList.filter((item) => item.name === productName);
-    return res;
+    let _arr = [];
+    for (let i = 0; i < productList.length; i++) {
+      const item = productList[i];
+      if (item.name === productName) {
+        _arr.push(item);
+      }
+    }
+    if (isStocked) {
+      return _arr.filter((item) => item.stocked === isStocked);
+    } else {
+      return _arr;
+    }
+  } else {
+    if (isStocked) {
+      return productList.filter((item) => item.stocked === isStocked);
+    } else {
+      return productList;
+    }
   }
-  return productList;
 }
 
 export { filterProductText };
